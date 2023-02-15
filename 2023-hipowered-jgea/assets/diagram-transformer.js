95ee03ba02158e04900d1203164a6501e8e1c82c
Eric Medvet <eric.medvet@gmail.com>
2023-02-15 09:49:39 +0100

    new hipowered talk

---
+++ 2023-hipowered-jgea/assets/diagram-transformer.js
@@ -0,0 +1,524 @@
+var DiagramTransformer = {
+  transform: function(code) {
+    var statements = code.split("\n");
+    var itemsCode = "";
+    var minX = Number.MAX_VALUE;
+    var maxX = -Number.MAX_VALUE;
+    var minY = Number.MAX_VALUE;
+    var maxY = -Number.MAX_VALUE;
+    for (var i = 0; i < statements.length; i++) {
+      var statement = statements[i].trim();
+      if (statement) {
+        var result = eval("this.shapes." + statement);
+        itemsCode += result.code;
+        minX = Math.min(result.minX, minX);
+        maxX = Math.max(result.maxX, maxX);
+        minY = Math.min(result.minY, minY);
+        maxY = Math.max(result.maxY, maxY);
+      }
+    }
+    var processedCode = "";
+    processedCode +=
+      '<svg width="' +
+      (maxX - minX + 2 * this.shapes.constants.padding) +
+      '" height="' +
+      (maxY - minY + 2 * this.shapes.constants.padding) +
+      '" role="img">';
+    processedCode +=
+      '<g transform="translate(' +
+      (-minX + this.shapes.constants.padding) +
+      "," +
+      (-minY + this.shapes.constants.padding) +
+      ')">';
+    processedCode += itemsCode;
+    processedCode += "</g>";
+    processedCode += "</svg>";
+    return processedCode;
+  },
+  transformAll: function(parent, className) {
+    var elements = parent.getElementsByClassName(className);
+    for (var i = 0; i < elements.length; i++) {
+      var code = elements[i].children[0].textContent;
+      var processed = this.transform(code);
+      elements[i].innerHTML = processed;
+    }
+  },
+  shapes: {
+    constants: {
+      padding: 5,
+      charHeight: 20,
+      charWidth: 12.5,
+      refRadius: 10,
+      cylYRadiusRatio: 0.075,
+      cursorWidth: 5,
+      cursorHeight: 10,
+      linkHeadSize: 10
+    },
+    ref: function(x, y, label, className) {
+      className = className ? className : "";
+      var code = "";
+      code +=
+        '<circle cx="' +
+        x +
+        '" cy="' +
+        y +
+        '" r="' +
+        this.constants.refRadius +
+        '" class="ref ' +
+        className +
+        '"/>';
+      if (label) {
+        code +=
+          '<text x="' +
+          x +
+          '" y="' +
+          (y - (this.constants.charHeight / 2 + this.constants.refRadius)) +
+          '" class="refLabel ' +
+          className +
+          '">' +
+          label +
+          "</text>";
+      }
+      return {
+        minX:
+          x -
+          Math.max(
+            (this.constants.charWidth * label.length) / 2,
+            this.constants.refRadius
+          ),
+        maxX:
+          x +
+          Math.max(
+            (this.constants.charWidth * label.length) / 2,
+            this.constants.refRadius
+          ),
+        minY:
+          y -
+          (this.constants.charHeight / 2 + this.constants.refRadius) -
+          this.constants.charHeight / 2,
+        maxY: y + this.constants.refRadius,
+        code: code
+      };
+    },
+    obj: function(x, y, w, h, typeLabel, contentLabel, className) {
+      className = className ? className : "";
+      var code = "";
+      code +=
+        '<rect x="' +
+        x +
+        '" y="' +
+        y +
+        '" width="' +
+        w +
+        '" height="' +
+        h +
+        '" class="obj ' +
+        className +
+        '"/>';
+      if (typeLabel) {
+        code +=
+          '<text x="' +
+          (x + w / 2) +
+          '" y="' +
+          (y - this.constants.charHeight / 2) +
+          '" class="typeLabel ' +
+          className +
+          '">' +
+          typeLabel +
+          "</text>";
+      }
+      if (contentLabel) {
+        code +=
+          '<text x="' +
+          (x + w / 2) +
+          '" y="' +
+          (y + 10 + this.constants.charHeight / 2) +
+          '" class="contentLabel ' +
+          className +
+          '">' +
+          contentLabel +
+          "</text>";
+      }
+      return {
+        minX:
+          x +
+          w / 2 -
+          Math.max((this.constants.charWidth * typeLabel.length) / 2, w / 2),
+        maxX:
+          x +
+          w / 2 +
+          Math.max((this.constants.charWidth * typeLabel.length) / 2, w / 2),
+        minY: y - this.constants.charHeight,
+        maxY: y + h,
+        code: code
+      };
+    },
+    cyl: function(x, y, w, h, label, className) {
+      className = className ? className : "";
+      var code = "";
+      code += "<g>";
+      code +=
+        '<path d="M ' +
+        x +
+        "," +
+        y +
+        " L " +
+        x +
+        "," +
+        (y + h) +
+        " A " +
+        w / 2 +
+        "," +
+        w * this.constants.cylYRadiusRatio +
+        " 0 0,0 " +
+        (x + w) +
+        "," +
+        (y + h) +
+        " L " +
+        (x + w) +
+        "," +
+        y +
+        " A " +
+        w / 2 +
+        "," +
+        w * this.constants.cylYRadiusRatio +
+        " 0 0,0 " +
+        x +
+        "," +
+        y +
+        " A " +
+        w / 2 +
+        "," +
+        w * this.constants.cylYRadiusRatio +
+        " 0 0,0 " +
+        (x + w) +
+        "," +
+        y +
+        '" class="cyl ' +
+        className +
+        '"/>';
+      if (label) {
+        code +=
+          '<text x="' +
+          (x + w / 2) +
+          '" y="' +
+          (y +
+            10 +
+            w * this.constants.cylYRadiusRatio +
+            this.constants.charHeight / 2) +
+          '" class="contentLabel ' +
+          className +
+          '">' +
+          label +
+          "</text>";
+      }
+      code += "</g>";
+      return {
+        minX: x,
+        maxX: x + w,
+        minY: y - w * this.constants.cylYRadiusRatio,
+        maxY: y + h + w * this.constants.cylYRadiusRatio,
+        code: code
+      };
+    },
+    arrow: function(x, y, w, h, a, className) {
+      className = className ? className : "";
+      a = a ? a : 0;
+      var code = "";
+      code +=
+        '<g transform="rotate(' +
+        a +
+        " " +
+        x +
+        " " +
+        y +
+        ')">' +
+        '<path d="M ' +
+        x +
+        "," +
+        y +
+        " m " +
+        -w / 4 +
+        "," +
+        -h / 2 +
+        " l 0," +
+        h / 2 +
+        " l " +
+        -w / 4 +
+        ",0" +
+        " l " +
+        w / 2 +
+        "," +
+        h / 2 +
+        " l " +
+        w / 2 +
+        "," +
+        -h / 2 +
+        " l " +
+        -w / 4 +
+        ",0" +
+        " l 0," +
+        -h / 2 +
+        'z" class="arrow ' +
+        className +
+        '"/></g>';
+      var r = Math.sqrt((w * w) / 4 + (h * h) / 4);
+      return {
+        minX: x - r,
+        maxX: x + r,
+        minY: y - r,
+        maxY: y + r,
+        code: code
+      };
+    },
+    array: function(x, y, w, h, n, cursor, eos, className) {
+      className = className ? className : "";
+      var code = "";
+      code += "<g>";
+      for (var i = 0; i < n; i++) {
+        code +=
+          '<rect x="' +
+          (x + w * i) +
+          '" y="' +
+          y +
+          '" width="' +
+          w +
+          '" height="' +
+          h +
+          '"' +
+          '" class="array ' +
+          (i == eos ? "eos " : "") +
+          className +
+          '"/>';
+      }
+      if (cursor !== "") {
+        code +=
+          '<path d="' +
+          "M " +
+          (x + w * cursor - this.constants.cursorWidth / 4) +
+          "," +
+          (y - this.constants.cursorHeight) +
+          " " +
+          "l " +
+          -this.constants.cursorWidth / 4 +
+          ",0 " +
+          "l 0," +
+          this.constants.cursorHeight / 2 +
+          " " +
+          "l " +
+          -this.constants.cursorWidth / 4 +
+          ",0 " +
+          "l " +
+          this.constants.cursorWidth / 2 +
+          "," +
+          this.constants.cursorHeight / 2 +
+          " " +
+          "l " +
+          this.constants.cursorWidth / 2 +
+          "," +
+          -this.constants.cursorHeight / 2 +
+          " " +
+          "l " +
+          -this.constants.cursorWidth / 4 +
+          ",0 " +
+          "l 0," +
+          -this.constants.cursorHeight / 2 +
+          " " +
+          'z" class="cursor ' +
+          className +
+          '"/>';
+      }
+      code += "</g>";
+      return {
+        minX: Math.min(
+          x,
+          cursor !== "" ? x + w * cursor - this.constants.cursorWidth : x
+        ),
+        maxX: Math.max(
+          x + w * n,
+          cursor !== "" ? x + w * cursor + this.constants.cursorWidth : x
+        ),
+        minY: y - ((cursor !== "") ? this.constants.cursorHeight : 0),
+        maxY: y + h,
+        code: code
+      };
+    },
+    zone: function(x, y, w, h, label, className) {
+      className = className ? className : "";
+      var code = "";
+      code +=
+        '<rect x="' +
+        x +
+        '" y="' +
+        y +
+        '" width="' +
+        w +
+        '" height="' +
+        h +
+        '" class="zone ' +
+        className +
+        '"/>';
+      if (label) {
+        code +=
+          '<text x="' +
+          (x + this.constants.charWidth) +
+          '" y="' +
+          (y - this.constants.charHeight / 2) +
+          '" class="zoneLabel ' +
+          className +
+          '">' +
+          label +
+          "</text>";
+      }
+      return {
+        minX: x,
+        maxX: Math.max(
+          x + w,
+          x + this.constants.charWidth + this.constants.charWidth * label.length
+        ),
+        minY: y - this.constants.charHeight,
+        maxY: y + h,
+        code: code
+      };
+    },
+    text: function(x, y, label, className) {
+      className = className ? className : "";
+      var code = "";
+      if (label) {
+        code +=
+          '<text x="' +
+          x +
+          '" y="' +
+          y +
+          '" class="textLabel ' +
+          className +
+          '">' +
+          label +
+          "</text>";
+      }
+      return {
+        minX: x - (this.constants.charWidth * label.length) / 2,
+        maxX: x + (this.constants.charWidth * label.length) / 2,
+        minY: y - this.constants.charHeight / 2,
+        maxY: y + y - this.constants.charHeight / 2,
+        code: code
+      };
+    },
+    link: function(coords, className) {
+      className = className ? className : "";
+      var minX = Number.MAX_VALUE;
+      var maxX = -Number.MAX_VALUE;
+      var minY = Number.MAX_VALUE;
+      var maxY = -Number.MAX_VALUE;
+      var code = "";
+      code += "<g>";
+      code += '<path d="';
+      for (var i = 0; i < Math.floor(coords.length / 2); i++) {
+        var command = "L";
+        if (i == 0) {
+          command = "M";
+        }
+        if (!isFinite(coords[i * 2]) && coords[i * 2].startsWith("j")) {
+          var lastX = parseFloat(coords[(i - 1) * 2]);
+          var lastY = parseFloat(coords[(i - 1) * 2 + 1]);
+          var radius = parseFloat(coords[i * 2].substring(1));
+          var direction = coords[i * 2 + 1];
+          if (direction == "n") {
+            code +=
+              "A" +
+              radius +
+              "," +
+              radius +
+              " 0 0,0 " +
+              lastX +
+              "," +
+              (lastY - 2 * radius);
+          } else if (direction == "e") {
+            code +=
+              "A" +
+              radius +
+              "," +
+              radius +
+              " 0 0,0 " +
+              (lastX + 2 * radius) +
+              "," +
+              lastY;
+          } else if (direction == "s") {
+            code +=
+              "A" +
+              radius +
+              "," +
+              radius +
+              " 0 0,0 " +
+              lastX +
+              "," +
+              (lastY + 2 * radius);
+          } else if (direction == "w") {
+            code +=
+              "A" +
+              radius +
+              "," +
+              radius +
+              " 0 0,0 " +
+              (lastX - 2 * radius) +
+              "," +
+              lastY;
+          }
+        } else {
+          var x = parseFloat(coords[i * 2]);
+          var y = parseFloat(coords[i * 2 + 1]);
+          minX = Math.min(minX, x);
+          maxX = Math.max(maxX, x);
+          minY = Math.min(minY, y);
+          maxY = Math.max(maxY, y);
+          code += command + x + "," + y + " ";
+        }
+      }
+      code += '" class="link ' + className + '"/>';
+      if (coords[coords.length - 1] == ">") {
+        code +=
+          '<g transform="rotate(' +
+          (Math.atan2(
+            coords[coords.length - 2] - coords[coords.length - 4],
+            coords[coords.length - 3] - coords[coords.length - 5]
+          ) /
+            Math.PI) *
+            180 +
+          " " +
+          coords[coords.length - 3] +
+          "," +
+          coords[coords.length - 2] +
+          ')">';
+        code +=
+          '<path d="' +
+          "M " +
+          coords[coords.length - 3] +
+          "," +
+          coords[coords.length - 2] +
+          " " +
+          "l " +
+          -this.constants.linkHeadSize +
+          "," +
+          -this.constants.linkHeadSize / 2 +
+          " " +
+          "l 0," +
+          this.constants.linkHeadSize +
+          " " +
+          'z" class="link head ' +
+          className +
+          '"/>';
+        code += "</g>";
+      }
+      code += "</g>";
+      return {
+        minX: minX,
+        maxX: maxX,
+        minY: minY,
+        maxY: maxY,
+        code: code
+      };
+    }
+  }
+};
+
+window.diagramTransformer = DiagramTransformer;
---
+++ 2023-hipowered-jgea/index.md
@@ -0,0 +1,435 @@
+class: middle, center
+
+# JGEA: a Modular Java Framework for Experimenting with Evolutionary Computation
+
+**Eric Medvet**<sup>1</sup>, Giorgia Nadizar<sup>2</sup>, Luca Manzoni<sup>2</sup>
+
+<font size="2"><sup>1</sup>Department of Engineering and Architecture, University of Trieste, Italy  <br/>
+<sup>2</sup>Department of Mathematics and Geosciences, University of Trieste, Italy  
+</font>
+
+HiPowerED meeting, Trieste, 15/2/2023
+
+---
+
+## Evolutionary computation
+
+What's the purpose of evolutionary computation?
+
+.center[**Optimization**!]
+
+That means, solving:
+$$\\argmin_{s \\in S} f(s)$$
+
+- for any $S$
+- for any $f$
+
+.note[Actually, even more general]
+
+---
+
+## Evolutionary computation in a nutshell
+
+.center.h25ex[![EC in a nuthsell](images/ec-nutshell.jpg)]
+
+Peculiarities:
+- great opportunity for parallelization
+  - fitness evaluations
+  - evolutionary runs (stochasticity)
+- heterogeneous concepts
+
+---
+
+## JGEA framework
+
+- Java (SE 18)
+  - Object-orientation $\rightarrow$ models well complex and variegate concepts
+  - Adequate syntactic constructs (generics, interfaces)
+  - Portable
+- Modular
+- Abstraction levels, down to implementation
+- Suite of benchmarks
+
+---
+
+## Structure and components
+
+EAs are a class of population-based optimization algorithms.
+
+As such, they **solve** optimization **problems**.
+
+We translate this into a hierarchy of interfaces, rooted in `Problem` and `Solver`.
+
+---
+
+## Problem hierarchy (1)
+
+```java
+public interface Problem<S> extends PartialComparator<S> {}
+```
+- `S` solution space
+- `PartialComparator<S>` to compare two solutions
+
+<br/>
+
+Solutions might be compared according to their *quality*:
+```java
+public interface QualityBasedProblem<S, Q> extends Problem<S> {
+  Function<S, Q> qualityFunction();
+  PartialComparator<Q> qualityComparator();
+}
+```
+- `Q` quality/fitness space
+- `Function<S, Q>` mapping a solution to a quality value
+- `PartialComparator<Q>` for comparing qualities
+
+---
+
+## Problem hierarchy (2)
+
+We might want to enforce *total ordering* among qualities:
+```java
+public interface TotalOrderQualityBasedProblem<S, Q> extends QualityBasedProblem<S, Q> {
+  Comparator<Q> totalOrderComparator();
+  @Override
+  default PartialComparator<Q> qualityComparator() { /*...*/ }
+}
+ ```
+- `Comparator<Q>` for comparing qualities (total ordering enforced)
+
+---
+
+## Problem hierarchy (3)
+
+Qualities might be *naturally comparable*:
+```java
+public interface ComparableQualityBasedProblem<S, Q extends Comparable<Q>>
+  extends TotalOrderQualityBasedProblem<S, Q> {
+  @Override
+  default Comparator<Q> totalOrderComparator() {
+    return Comparable::compareTo;
+  }
+}
+```
+- Qualities are compared according to their natural ordering
+---
+
+## Problem hierarchy (recap)
+
+```java
+public interface Problem<S> extends PartialComparator<S> {}
+```
+$$\downarrow$$
+```java
+public interface QualityBasedProblem<S, Q> extends Problem<S> {}
+```
+$$\downarrow$$
+```java
+public interface TotalOrderQualityBasedProblem<S, Q> extends QualityBasedProblem<S, Q> {}
+```
+$$\downarrow$$
+```java
+public interface ComparableQualityBasedProblem<S, Q extends Comparable<Q>>
+  extends TotalOrderQualityBasedProblem<S, Q> {}
+```
+
+---
+
+## Solver hierarchy (1)
+
+```java
+public interface Solver<P extends Problem<S>, S> {
+  Collection<S> solve(
+      P problem,
+      RandomGenerator random,
+      ExecutorService executor
+  ) throws SolverException;
+}
+```
+- Solves problem of type `P`
+- Provides the caller with solutions of type `S`
+- `RandomGenerator` for *repeatability*
+- `ExecutorService` for distributing computation (aimed at *efficiency*)
+- *Stateless* nature
+
+---
+
+## Solver hierarchy (2)
+
+All relevant EAs share an *iterative* structure:
+
+```java
+public interface IterativeSolver<T, P extends Problem<S>, S> extends Solver<P, S> {
+  default Collection<S> solve(
+      P problem, RandomGenerator random,
+      ExecutorService executor, Listener<? super T> listener
+  ) throws SolverException {
+    T state = `init`(problem, random, executor);
+    listener.listen(state);
+    while (!`terminate`(problem, random, executor, state)) {
+      `update`(problem, random, executor, state);
+      listener.listen((T) state.immutableCopy());
+    }
+    listener.done();
+    return `extractSolutions`(problem, random, executor, state);
+  }
+}
+```
+- `T` state $\rightarrow$ different solvers might require to store different structures
+
+---
+
+## Solver hierarchy (3)
+
+```java
+public abstract class AbstractPopulationBasedIterativeSolver<
+    T extends POSetPopulationState<G, S, Q>, P extends QualityBasedProblem<S, Q>, G, S, Q>
+    implements IterativeSolver<T, P, S> {
+  /*...*/
+}
+```
+- `init()` $\rightarrow$ initialize the state and sample initial population
+- `terminate()` $\rightarrow$ check termination condition
+- `extractSolutions()` $\rightarrow$ take the best individual(s) from the population
+<br/><br/>
+- `update()` not implemented $\rightarrow$ characterizes each EA
+
+---
+
+## Solver hierarchy (recap)
+
+```java
+public interface Solver<P extends Problem<S>, S> {}
+```
+$$\downarrow$$
+```java
+public interface IterativeSolver<T, P extends Problem<S>, S> extends Solver<P, S> {}
+```
+$$\downarrow$$
+```java
+public abstract class AbstractPopulationBasedIterativeSolver<
+    T extends POSetPopulationState<G, S, Q>, P extends QualityBasedProblem<S, Q>, G, S, Q>
+    implements IterativeSolver<T, P, S> {}
+```
+
+---
+
+## Individual
+
+Capture genotype-phenotype representation
+```java
+public record Individual<G, S, Q>(
+    G genotype,
+    S solution,
+    Q fitness,
+    long fitnessMappingIteration,
+    long genotypeBirthIteration
+)
+```
+
+---
+
+## Individual creation
+
+1. Obtain a genotype
+
+- *From scratch*
+
+```java
+public interface Factory<T> {
+  List<T> build(int n, RandomGenerator random);
+}
+```
+  - *With genetic operators*
+
+```java
+public interface GeneticOperator<G> {
+  List<? extends G> apply(List<? extends G> parents, RandomGenerator random);
+  int arity();
+}
+```
+
+<ol start="2">
+  <li>Map it to a phenotype</li>
+  <li>Evaluate its fitness</li>
+</ol>
+
+---
+
+## Individual selection
+
+Selection for reproduction or survival
+```java
+public interface Selector<T> {
+  <K extends T> K select(
+      PartiallyOrderedCollection<K> ks,
+      RandomGenerator random
+  );
+}
+```
+
+---
+
+## Listener
+
+Need to *track* the execution of the solver, extracting and saving information during a run:
+```java
+public interface Listener<E> {
+  void listen(E e);
+  default void done() {}
+}
+```
+
+Used to `listen()` to updates of the state during an execution of the `solve()` method.
+
+A `ListenerFactory` can build *augmented* listeners:
+```java
+public interface ListenerFactory<E, K> {
+  Listener<E> build(K k);
+}
+```
+
+---
+
+## Listener examples
+
+.cols[
+.col60[.center.h40ex[![Tabular printer](images/tabular-printer.png)]
+]
+.col40[.center.h40ex[![Telegram updater](images/telegram.jpg)]
+]
+]
+
+---
+
+## Case study 1: experimenting with JGEA
+
+Testing the **solidity** of JGEA.
+
+1. Problem definition
+  - Choose solution space `S`
+  - Decide which `Problem` to implement (along the hierarchy)
+2. Choose an appropriate solver
+  - Select an encoding (i.e., a genotype `G`)
+  - Define a genotype-phenotype mapping
+  - Select an appropriate EA for the chosen genotype
+3. Carry out the experimental evaluation
+  - (Optional) Choose a `Listener` to monitor the execution
+
+---
+
+## CS 1: solid and *scalable*
+
+.cols[
+.c75[
+```java
+public record LocomotionProblem(
+    Function<Robot, Outcome> qualityFunction,
+    Comparator<Outcome> totalOrderComparator
+) implements TotalOrderQualityBasedProblem<Robot, Outcome> {}
+```
+]
+.c10[![Robot images](images/robot.png)]
+]
+
+<p align="center">
+  <img src="images/scalability.png" alt="Scalability example."/>
+</p>
+
+---
+
+## Case study 2: adding a new EA to JGEA
+
+Testing the **extensibility** of JGEA.
+
+1. Formally define the EA
+  - Define it in natural language or in pseudo-code
+2. Frame the EA as a class extending `AbstractPopulationBasedIterativeSolver`
+  - Match subroutines like `init()`, `terminate()`, `update()`, and `extractSolutions()`
+3. Implement the class
+  - Choose the allowed generics parameters in the class signature
+  - Identify any additional elements needed (as fields of the class)
+  - Implement the `update()` method (and `@Override` other methods if needed)
+  to give functionalities
+4. Test the implemented EA on a benchmark
+
+---
+
+## CS 2: re-implementing Map Elites
+
+.cols[
+.c30[![Map Elites pseudocode](images/mapelites.png)]
+.c70[
+```java
+public class MapElites<G,
+    P extends QualityBasedProblem<S, Q>,
+    S, Q> extends AbstractPopulationBasedIterativeSolver
+    <MapElites.State<G, S, Q>, P, G, S, Q> {
+  private final Mutation<G> mutation;
+  private final Function<Individual<G, S, Q>,
+        List<Double>> featuresExtractor;
+  private final List<MapOfElites.Feature> features;
+  /***/
+  @Override
+  public void update(P problem,
+      RandomGenerator random,
+      ExecutorService executor,
+      State<G, S, Q> state
+  ) throws SolverException {
+    /***/
+    state.mapOfElites.addAll(offspring);
+    /***/
+  }
+}
+```
+]
+]
+
+---
+
+## Summing up... did I convince you?
+
+JGEA is
+- Solid <span>&#10004;</span>
+- Extensible <span>&#10004;</span>
+
+Thanks to modularity, abstraction, and a clear modeling of concepts.
+
+<br/><br/>
+
+And if you liked it, you're welcome to join us
+.cols[
+.c50[
+<i class="fa fa-github" aria-hidden="true"></i>
+[github.com/ericmedvet/jgea](https://github.com/ericmedvet/jgea)
+]
+.c50[
+<i class="fa fa-file-text" aria-hidden="true"></i>
+[medvet.inginf.units.it/jgea/](https://medvet.inginf.units.it/jgea/)
+]
+]
+
+---
+
+class: center
+
+## Recap
+
+.cols[
+.c50[
+.h40ex[![Paper mini img](images/paper.png)]
+
+]
+.c50[
+.center[[github.com/ericmedvet/jgea](https://github.com/ericmedvet/jgea)]
+
+Authors/contacts:
+
+.h10ex[![Eric Medvet](images/author-medvet.jpg)]
+.h10ex[![Giorgia Nadizar](images/author-nadizar.jpg)]
+.h10ex[![Luca Manzoni](images/author-manzoni.jpg)]
+
+<i class="fa fa-envelope" aria-hidden="true"></i> [emedvet@units.it](mailto:emedvet@units.it)  
+<i class="fa fa-twitter" aria-hidden="true"></i> [@EricMedvetTs](https://twitter.com/EricMedvetTs)  
+]
+]
